6a9b4b95b6b01f2f950a3ae8c81929e0ea86eeda
Sandra Chaparro <sandra.saldepaz@gmail.com>
2026-05-22 12:47:29 -0500

    test for blog no posted

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,10 @@
 
 export default new Map([
-["src/content/blogs/astro-vs-gatsby.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-gatsby.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/astro-vs-nextjs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-nextjs.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/astro-component.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-component.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/the-journey-of-life.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-journey-of-life.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/astro-vs-gatsby.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-gatsby.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/the-power-of-mindfulness.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-power-of-mindfulness.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/the-essence-of-love.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-essence-of-love.mdx&astroContentModuleFlag=true")]]);
+["src/content/blogs/the-journey-of-life.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-journey-of-life.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/astro-vs-nextjs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-nextjs.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/AI Governance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FAI+Governance.mdx&astroContentModuleFlag=true")]]);
 		
