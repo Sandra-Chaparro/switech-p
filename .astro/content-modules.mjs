1e059968a9ebe755d3225c00b1a06b41d87eb705
Sandra Chaparro <sandra.saldepaz@gmail.com>
2026-05-22 13:04:22 -0500

    templateBlog creation

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -6,5 +6,6 @@ export default new Map([
 ["src/content/blogs/the-journey-of-life.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-journey-of-life.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/astro-vs-nextjs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-nextjs.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/AI Governance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FAI+Governance.mdx&astroContentModuleFlag=true")]]);
+["src/content/blogs/AI Governance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FAI+Governance.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/BlogTemplate.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FBlogTemplate.mdx&astroContentModuleFlag=true")]]);
 		
