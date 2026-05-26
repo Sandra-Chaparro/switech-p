10861f4eee11ff4e066b05e0b8d00d60b664292a
Sandra Chaparro <sandra.saldepaz@gmail.com>
2026-05-25 22:02:13 -0500

    footer change and ptf removed

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,7 @@
 
 export default new Map([
-["src/content/blogs/astro-component.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-component.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/astro-vs-gatsby.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-gatsby.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/the-power-of-mindfulness.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-power-of-mindfulness.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/the-journey-of-life.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fthe-journey-of-life.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/astro-vs-nextjs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-nextjs.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/BlogTemplate.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FBlogTemplate.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/AI Governance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FAI+Governance.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/BlogTemplate.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FBlogTemplate.mdx&astroContentModuleFlag=true")]]);
+["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")]]);
 		
