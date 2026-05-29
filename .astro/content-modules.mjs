db2e3b9f34185ad6be09508ef536f5cecdaa4f38
Sandra Chaparro <sandra.saldepaz@gmail.com>
2026-05-29 14:07:38 -0500

    fix logo on header

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/blogs/astro-vs-gatsby.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-gatsby.mdx&astroContentModuleFlag=true")],
 ["src/content/blogs/BlogTemplate.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FBlogTemplate.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")],
-["src/content/blogs/AI Governance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FAI+Governance.mdx&astroContentModuleFlag=true")]]);
+["src/content/blogs/AI Governance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2FAI+Governance.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/astro-vs-gatsby.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fastro-vs-gatsby.mdx&astroContentModuleFlag=true")],
+["src/content/blogs/introduction-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblogs%2Fintroduction-to-astro.mdx&astroContentModuleFlag=true")]]);
 		
